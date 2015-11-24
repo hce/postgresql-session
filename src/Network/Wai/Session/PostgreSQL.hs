74beab19e4a6051b3144a662cea017ae72d631c7
Hans-Christian Esperer <hc@hcesperer.org>
2015-11-24 14:52:52 +0100

    Add timeout parameter

--- src/Network/Wai/Session/PostgreSQL.hs
+++ src/Network/Wai/Session/PostgreSQL.hs
@@ -1,6 +1,7 @@
 module Network.Wai.Session.PostgreSQL
     ( dbStore
     , WithPostgreSQLConn (..)
+    , StoreSettings (..)
     ) where
 
 import Control.Exception.Base
@@ -15,6 +16,11 @@ import Network.Wai.Session
 
 import qualified Data.ByteString as B
 
+data StoreSettings = StoreSettings
+    { storeSettingsSessionTimeout :: Int64
+    , storeSettingsKeyGen :: IO B.ByteString
+    }
+
 class WithPostgreSQLConn a where
     withPostgreSQLConn :: a -> (Connection -> IO b) -> IO b
 
@@ -24,33 +30,34 @@ instance WithPostgreSQLConn Connection where
 qryCreateTable      = "CREATE TABLE session (id bigserial NOT NULL, session_key character varying NOT NULL, session_created_at bigint NOT NULL, session_last_access bigint NOT NULL, session_value bytea NOT NULL, CONSTRAINT session_pkey PRIMARY KEY (id), CONSTRAINT session_session_key_key UNIQUE (session_key)) WITH (OIDS=FALSE);"
 qryCreateSession    = "INSERT INTO session (session_key, session_created_at, session_last_access, session_value) VALUES (?,?,?,?)"
 qryUpdateSession    = "UPDATE session SET session_value=?,session_last_access=? WHERE session_key=?"
-qryLookupSession    = "SELECT session_value FROM session WHERE session_key=?"
+qryLookupSession    = "SELECT session_value FROM session WHERE session_key=? AND session_last_access>=?"
 qryLookupSession'   = "UPDATE session SET session_last_access=? WHERE session_key=?"
+qryLookupSession''  = "SELECT session_value FROM session WHERE session_key=?"
 
-dbStore :: (WithPostgreSQLConn a, Serialize k, Eq k, Serialize v, MonadIO m) => a -> IO B.ByteString -> IO (SessionStore m k v)
-dbStore pool gen = do
+dbStore :: (WithPostgreSQLConn a, Serialize k, Eq k, Serialize v, MonadIO m) => a -> StoreSettings -> IO (SessionStore m k v)
+dbStore pool stos = do
     withPostgreSQLConn pool $ \ conn -> do
         unerror $ execute_ conn qryCreateTable
-    return $ dbStore' pool gen
+    return $ dbStore' pool stos
 
-dbStore' :: (WithPostgreSQLConn a, Serialize k, Eq k, Serialize v, MonadIO m) => a -> IO B.ByteString -> SessionStore m k v
-dbStore' pool gen Nothing = do
-    newKey <- gen
+dbStore' :: (WithPostgreSQLConn a, Serialize k, Eq k, Serialize v, MonadIO m) => a -> StoreSettings -> SessionStore m k v
+dbStore' pool stos Nothing = do
+    newKey <- storeSettingsKeyGen stos
     let map     = [] :: [(k, v)]
         map'    = "" -- encode map
     curtime <- round <$> liftIO getPOSIXTime
     withPostgreSQLConn pool $ \ conn ->
         void $ execute conn qryCreateSession (newKey, curtime :: Int64, curtime, map' :: B.ByteString)
     backend pool newKey map
-dbStore' pool gen (Just key) = do
+dbStore' pool stos (Just key) = do
     let map     = [] :: [(k, v)]
         map'    = "\"\"" -- encode map
     curtime <- round <$> liftIO getPOSIXTime
     res <- withPostgreSQLConn pool $ \ conn ->
-        query conn qryLookupSession (Only key) :: IO [Only B.ByteString]
+        query conn qryLookupSession (key, curtime - storeSettingsSessionTimeout stos) :: IO [Only B.ByteString]
     case res of
         [Only _]    -> backend pool key map
-        _           -> dbStore' pool gen Nothing
+        _           -> dbStore' pool stos Nothing
 
 backend :: (WithPostgreSQLConn a, Serialize k, Eq k, Serialize v, MonadIO m) => a -> B.ByteString -> [(k, v)] -> IO (Session m k v, IO B.ByteString)
 backend pool key mappe = do
@@ -64,7 +71,7 @@ reader pool key mappe k = do
     curtime <- round <$> liftIO getPOSIXTime
     res <- liftIO $ withPostgreSQLConn pool $ \conn -> do
         void $ execute conn qryLookupSession' (curtime :: Int64, key)
-        query conn qryLookupSession (Only key)
+        query conn qryLookupSession'' (Only key)
     res' <- case res of
         [Only store']    -> case decode (fromBinary store') of
             Right store     -> return $ k `lookup` store
@@ -76,7 +83,7 @@ writer :: (WithPostgreSQLConn a, Serialize k, Eq k, Serialize v, MonadIO m) => a
 writer pool key mappe k v = do
     curtime <- round <$> liftIO getPOSIXTime
     [Only store] <- liftIO $ withPostgreSQLConn pool $ \conn ->
-        query conn qryLookupSession (Only key)
+        query conn qryLookupSession'' (Only key)
     let store'      = case decode (fromBinary store) of
             Right s             -> s
             _                   -> []
