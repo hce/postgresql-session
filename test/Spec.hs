df1492e32dd36bbf445b86f218f938aca5d519e9
Simon Hengel <sol@typeful.net>
2020-09-25 00:20:16 +0700

    Add comments to tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -19,15 +19,18 @@ spec = describe "Network.Wai.Session.PostgreSQL" $ it "handles sessions" $ do
     store <- dbStore conn' testSettings
     purger conn' testSettings
 
+    -- new session
     ((lookupSess1, insertSess1), mknewsessid) <- store Nothing
     sessid <- mknewsessid
 
+    -- insert
     insertSess1 ("foo" :: B.ByteString) ("bar" :: B.ByteString)
-
     lookupSess1 "foo" `shouldReturn` Just "bar"
 
+    -- non-existing key
     lookupSess1 "bar" `shouldReturn` Nothing
 
+    -- existing session
     ((lookupSess2, insertSess2), mknewsessid) <- store $ Just sessid
     newsessid <- mknewsessid
 
@@ -35,6 +38,7 @@ spec = describe "Network.Wai.Session.PostgreSQL" $ it "handles sessions" $ do
 
     newsessid `shouldBe` sessid
 
+    -- invalid session
     let invalidsessid = "foobar"
     ((lookupSess3, insertSess3), mknewsessid) <- store $ Just invalidsessid
     newsessid2 <- mknewsessid
@@ -44,11 +48,12 @@ spec = describe "Network.Wai.Session.PostgreSQL" $ it "handles sessions" $ do
 
     lookupSess3 "foo" `shouldReturn` Nothing
 
+    -- re-accessing session
     ((lookupSess4, insertSess4), mknewsessid) <- store $ Just sessid
     lookupSess4 "foo" `shouldReturn` Just "bar"
 
+    -- purged session
     threadDelay 2000000
-
     ((lookupSess5, insertSess5), mknewsessid) <- store $ Just sessid
     lookupSess5 "foo" `shouldReturn` Nothing
 
